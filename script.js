ec5f1050e1e2f365c5a48195866af082f3115aa9
izabeliFurtuoso <166657032+izabeliFurtuoso@users.noreply.github.com>
2025-03-24 11:46:29 -0300

    .

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const aumentaFonteBotao  = document.getElementById('aumentar-fonte');
 const diminuirFonteBotao  = document.getElementById('diminuir-fonte');
 
 let tamanhoAtualFonte = 1;
+
 aumentaFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte += 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
@@ -15,4 +16,5 @@ diminuirFonteBotao.addEventListener('clik', function(){
 
    })
    
+
 })
