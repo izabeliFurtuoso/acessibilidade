d1e017b0250c55d89e91d1e6fef83cab57cf7674
izabeliFurtuoso <166657032+izabeliFurtuoso@users.noreply.github.com>
2025-03-31 11:29:43 -0300

    .

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-document.addEventListener('DOMContentLoaded', function(){
-const aumentaFonteBotao  = document.getElementById('aumentar-fonte');
-const diminuirFonteBotao  = document.getElementById('diminuir-fonte');
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-let tamanhoAtualFonte = 1;
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-aumentaFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    })
 
-})
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-diminuirFonteBotao.addEventListener('clik', function(){
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    const alternaContraste = document.getElementById('alterna-contraste')
 
-   })
-   
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
 
 })
